7b0b15aa38a5c724af67b57888864dd789b900c1
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2025-08-04 09:45:41 -0600

    Create function to populate grid dynamically Define a function that creates gridDivSize * gridDivSize div elements. Each one is given a class and a number. This prepares the interactive grid.

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 let container = document.querySelector(".container");
 const gridDivSize = 16;
+
+function createContainerDivs () {
+    for (let i = 0; i < gridDivSize * gridDivSize; i++){
+        const gridDivItem = document.createElement("div");
+        gridDivItem.classList.add("dynamic-div-item");
+        gridDivItem.textContent = i + 1;
+        container.appendChild(gridDivItem);
+    }
+}
