3782fcb5114c2244d5d4fb3added2b7e08c5e363
Moses Butera <152932522+MosesButera@users.noreply.github.com>
2025-08-05 02:22:23 -0600

    Add function to generate random number Create a reusable function that returns a random integer between 0 and the given number (inclusive). Used to generate random RGB values.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Create the 16*16 grid dynamically
+
 let container = document.querySelector(".container");
 const gridDivSize = 16;
 
@@ -5,9 +7,17 @@ function createContainerDivs () {
     for (let i = 0; i < gridDivSize * gridDivSize; i++){
         const gridDivItem = document.createElement("div");
         gridDivItem.classList.add("dynamic-div-item");
-        gridDivItem.textContent = i + 1;
+        // gridDivItem.textContent = i + 1;
         container.appendChild(gridDivItem);
     }
 }
 
 createContainerDivs();
+
+function random(number) {
+    return Math.floor(Math.random() * (number + 1));
+}
+
+
+
+
